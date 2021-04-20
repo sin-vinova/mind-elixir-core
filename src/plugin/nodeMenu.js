485359193917040f47d3cce3be86ee4923a6f072
sin <sin@vinova.com.sg>
2021-04-20 15:07:11 +0700

    [UI] - build

--- src/plugin/nodeMenu.js
+++ src/plugin/nodeMenu.js
@@ -266,8 +266,6 @@ export default function (mind) {
   }
   let bgOrFont
   let styleDiv = createDiv('nm-style', 'style')
-  // let tagDiv = createDiv('nm-tag', 'tag')
-  // let iconDiv = createDiv('nm-icon', 'icon')
   let colorList
   if (mind.colorList) {
     colorList = mind.colorList
@@ -304,23 +302,10 @@ export default function (mind) {
           .join('')}
       </div>
   `
-  // tagDiv.innerHTML = `
-  //     ${i18n[locale].tag}<input class="nm-tag" tabindex="-1" placeholder="${i18n[locale].tagsSeparate}" /><br>
-  // `
-  // iconDiv.innerHTML = `
-  //     ${i18n[locale].icon}<input class="nm-icon" tabindex="-1" placeholder="${i18n[locale].iconsSeparate}" /><br>
-  // `
 
   let menuContainer = document.createElement('nmenu')
   menuContainer.className="nmenu-verion-2"
-  // menuContainer.innerHTML = `
-  // <div class="button-container"><svg class="icon" aria-hidden="true">
-  // <use xlink:href="#icon-close"></use>
-  // </svg></div>
-  // `
   menuContainer.appendChild(styleDiv)
-  // menuContainer.appendChild(tagDiv)
-  // menuContainer.appendChild(iconDiv)
   menuContainer.hidden = true
 
   function clearSelect(klass, remove) {
@@ -331,12 +316,6 @@ export default function (mind) {
   }
 
   mind.container.append(menuContainer)
-  // let sizeSelector = menuContainer.querySelectorAll('.size')
-  // let bold = menuContainer.querySelector('.bold')
-  // let buttonContainer = menuContainer.querySelector('.button-container')
-  // let fontBtn = menuContainer.querySelector('.font')
-  // let tagInput = document.querySelector('.nm-tag')
-  // let iconInput = document.querySelector('.nm-icon')
   menuContainer.onclick = e => {
     if (!mind.currentNode) return
     let nodeObj = mind.currentNode.nodeObj
@@ -344,9 +323,6 @@ export default function (mind) {
       if (!nodeObj.style) nodeObj.style = {}
       clearSelect('.palette', 'nmenu-selected')
       e.target.className = 'palette nmenu-selected'
-      // if (bgOrFont === 'font') {
-      //   nodeObj.style.color = e.target.dataset.color
-      // } else if (bgOrFont === 'background') {
       nodeObj.style.background = e.target.dataset.color
 
       // update background node
@@ -367,7 +343,11 @@ export default function (mind) {
         }
       }
 
-      if (nodeObj.children && nodeObj.children.length !== 0) {
+      if (nodeObj.root) {
+        nodeObj.style = {
+          background: e.target.dataset.color,
+        }
+      } else if (nodeObj.children && nodeObj.children.length !== 0) {
         nodeObj.children = nodeObj.children.map(childNode => {
           childNode.style = {
             background: e.target.dataset.color,
@@ -376,83 +356,9 @@ export default function (mind) {
           return childNode
         })
       }
-      // }
       mind.updateNodeStyle(nodeObj)
-      // } else if (e.target.className === 'background') {
-      //   clearSelect('.palette', 'nmenu-selected')
-      //   bgOrFont = 'background'
-      //   e.target.className = 'background selected'
-      //   e.target.previousElementSibling.className = 'font'
-      //   if (nodeObj.style && nodeObj.style.background)
-      //     menuContainer.querySelector(
-      //       '.palette[data-color="' + nodeObj.style.background + '"]'
-      //     ).className = 'palette nmenu-selected'
-      // } else if (e.target.className === 'font') {
-      //   clearSelect('.palette', 'nmenu-selected')
-      //   bgOrFont = 'font'
-      //   e.target.className = 'font selected'
-      //   e.target.nextElementSibling.className = 'background'
-      //   if (nodeObj.style && nodeObj.style.color)
-      //     menuContainer.querySelector(
-      //       '.palette[data-color="' + nodeObj.style.color + '"]'
-      //     ).className = 'palette nmenu-selected'
     }
   }
-  // Array.from(sizeSelector).map(
-  //   dom =>
-  //     (dom.onclick = e => {
-  //       if (!mind.currentNode.nodeObj.style) mind.currentNode.nodeObj.style = {}
-  //       clearSelect('.size', 'size-selected')
-  //       let size = e.currentTarget
-  //       mind.currentNode.nodeObj.style.fontSize = size.dataset.size
-  //       size.className = 'size size-selected'
-  //       mind.updateNodeStyle(mind.currentNode.nodeObj)
-  //     })
-  // )
-  // bold.onclick = e => {
-  //   console.log('bold', mind.currentNode)
-  //   if (!mind.currentNode.nodeObj.style) mind.currentNode.nodeObj.style = {}
-  //   if (mind.currentNode.nodeObj.style.fontWeight === 'bold') {
-  //     delete mind.currentNode.nodeObj.style.fontWeight
-  //     e.currentTarget.className = 'bold'
-  //     mind.updateNodeStyle(mind.currentNode.nodeObj)
-  //   } else {
-  //     mind.currentNode.nodeObj.style.fontWeight = 'bold'
-  //     e.currentTarget.className = 'bold size-selected'
-  //     mind.updateNodeStyle(mind.currentNode.nodeObj)
-  //   }
-  // }
-  // tagInput.onchange = e => {
-  //   console.log('tagInput', mind.currentNode)
-  //   if (!mind.currentNode) return
-  //   if (!e.target.value) {
-  //     mind.currentNode.nodeObj.tags = []
-  //   } else {
-  //     mind.currentNode.nodeObj.tags = e.target.value.split(',')
-  //   }
-  //   mind.updateNodeTags(mind.currentNode.nodeObj)
-  // }
-  // iconInput.onchange = e => {
-  //   if (!mind.currentNode) return
-  //   mind.currentNode.nodeObj.icons = e.target.value.split(',')
-  //   mind.updateNodeIcons(mind.currentNode.nodeObj)
-  // }
-  // let state = 'open'
-  // buttonContainer.onclick = e => {
-  //   if (state === 'open') {
-  //     state = 'close'
-  //     menuContainer.className = 'close'
-  //     buttonContainer.innerHTML = `<svg class="icon" aria-hidden="true">
-  //   <use xlink:href="#icon-menu"></use>
-  //   </svg>`
-  //   } else {
-  //     state = 'open'
-  //     menuContainer.className = ''
-  //     buttonContainer.innerHTML = `<svg class="icon" aria-hidden="true">
-  //   <use xlink:href="#icon-close"></use>
-  //   </svg>`
-  //   }
-  // }
   mind.bus.addListener('unselectNode', function () {
     menuContainer.hidden = true
   })
@@ -464,38 +370,11 @@ export default function (mind) {
     } else {
       menuContainer.hidden = true
     }
-    // menuContainer.hidden = false
-    // E(nodeObj.id).scrollIntoView({ behavior: "smooth", block: "center", inline: "center" })
     if (mind.onRedirectPath && mind.isRedirectPath) {
       mind.onRedirectPath(nodeObj)
     }
     clearSelect('.palette', 'nmenu-selected')
     clearSelect('.size', 'size-selected')
     clearSelect('.bold', 'size-selected')
-    // bgOrFont = 'font'
-    // fontBtn.className = 'font selected'
-    // fontBtn.nextElementSibling.className = 'background'
-    // if (nodeObj.style) {
-    //   if (nodeObj.style.fontSize)
-    //     menuContainer.querySelector(
-    //       '.size[data-size="' + nodeObj.style.fontSize + '"]'
-    //     ).className = 'size size-selected'
-    //   if (nodeObj.style.fontWeight)
-    //     menuContainer.querySelector('.bold').className = 'bold size-selected'
-    //   if (nodeObj.style.color)
-    //     menuContainer.querySelector(
-    //       '.palette[data-color="' + nodeObj.style.color + '"]'
-    //     ).className = 'palette nmenu-selected'
-    // }
-    // if (nodeObj.tags) {
-    //   tagInput.value = nodeObj.tags.join(',')
-    // } else {
-    //   tagInput.value = ''
-    // }
-    // if (nodeObj.icons) {
-    //   iconInput.value = nodeObj.icons.join(',')
-    // } else {
-    //   iconInput.value = ''
-    // }
   })
 }
